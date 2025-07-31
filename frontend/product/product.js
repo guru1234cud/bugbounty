7a7a2450b55479fb130ca67ffa83cc7c33c16a14
guru1234cud <guru2006may@email.com>
2025-07-31 16:02:35 +0530

    dsds

--- frontend/product/product.js
+++ frontend/product/product.js
@@ -2,15 +2,13 @@
       const product = document.getElementById("location-select").value;
 
       try {
-        const response = await fetch("http://localhost:3000/product_count", {
+        const response = await fetch("/product_count", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
           },
           body: JSON.stringify({ product })
-          
         });
-
         const data = await response.json();
         console.log(data);
         
